b6960f28a08580db473c025a9a5c208d316a3aac
winfred <achiengwinfred20@gmail.com>
2024-03-18 10:41:34 +0300

    user entity

---
+++ src/main/java/com/winfred/security/model/User.java
@@ -0,0 +1,28 @@
+package com.winfred.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+}
